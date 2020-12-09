844192632fd4f6cdd80a6f782688d1cf53f14022
Oxezyn <47291780+leonld94@users.noreply.github.com>
2020-12-09 20:54:06 +0900

    회원가입 오류 구현
    성현이가 exists 찾아서 됨
    성현이 사기캐임

--- CherryPickingPicture/CherryPickingPicture/views.py
+++ CherryPickingPicture/CherryPickingPicture/views.py
@@ -12,22 +12,42 @@ def main(request):
 
 
 def login(request):
-    if request.method == "POST":
+    if request.COOKIES.get('username') is not None:
+        username = request.COOKIES.get('username')
+        password = request.COOKIES.get('password')
+        user = auth.authenticate(request, username=username, password=password)
+        if user is not None:
+            auth.login(request, user)
+            return redirect("../")
+        else:
+            return render(request, "account/login.html")
+    elif request.method == "POST":
         un = request.POST['id'];
         passwd = request.POST['password'];
         user = auth.authenticate(request, username=un, password = passwd);
         if user is not None:
             auth.login(request, user)
-            return redirect('');
+            if request.POST.get("keep_login") == "TRUE":
+                response = render(request, 'Main_page.html')
+                response.set_cookie('username', un)
+                response.set_cookie('password', passwd)
+                return response
+            return redirect('../');
     return render(request, 'Login.html');
 
 
 def signup(request):
     if request.method == "POST":
-        if request.POST['password1'] == request.POST['password2']:
+        if User.objects.filter(username = request.POST['id']).exists():
+            context = {'failure' : "이미 존재하는 아이디입니다."}
+            return render(request ,'Signup.html', context);
+        elif request.POST['password1'] == request.POST['password2']:
             user = User.objects.create_user(
                 username=request.POST['id'], password=request.POST['password1'], first_name=request.POST['nickname']
             )
-            return render(request, 'Login.html');
-            #return redirect('login');
+            #return render(request, 'Login.html');
+            return redirect('../Login');
+        elif request.POST['password1'] != request.POST['password2']:
+            context = {'failure' : "비밀번호가 일치하지 않습니다."}
+            return render(request, 'Signup.html', context);
     return render(request, 'Signup.html');
